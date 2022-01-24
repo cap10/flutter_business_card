bcbf5ce6cffcd047b71e8a0f3dc9c55782fd33fb
cap10 <takaiteyi@gmail.com>
2022-01-24 17:36:23 +0200

    added image, contact information

--- lib/main.dart
+++ lib/main.dart
@@ -14,29 +14,51 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/example.jpg'),
+              ),
+              Text(
+                'Venon Mapfunde',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                height: 10.0,
+                child: Divider(
+                  color: Colors.teal.shade900,
+                ),
+              ),
               Container(
                 color: Colors.white,
-                width: 100.00,
                 height: 100.00,
-                child: Text('Container 1'),
+                width: double.infinity,
+                child: Text(
+                  '+263775091262',
+                  style: TextStyle(color: Colors.teal.shade100, fontSize: 20.0),
+                ),
               ),
               SizedBox(
                 height: 10.0,
               ),
               Container(
-                color: Colors.red,
-                width: 100.0,
+                color: Colors.white,
                 height: 100.00,
-                child: Text('Container 2'),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                color: Colors.blue,
-                width: 100.0,
-                height: 100.00,
-                child: Text('Container 3'),
+                width: double.infinity,
+                child: Text(
+                  'takaiteyi@gmail.com',
+                  style: TextStyle(color: Colors.teal.shade100, fontSize: 20.0),
+                ),
               )
             ],
           ),
